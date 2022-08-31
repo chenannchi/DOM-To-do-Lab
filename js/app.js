5cfff33038dd176180998017e446001db29559c1
chenannchi <anchichenusa@gmail.com>
2022-08-30 23:37:39 -0400

    finish level up challenge 1 ~ 4

--- js/app.js
+++ js/app.js
@@ -1,15 +1,36 @@
 let input = document.getElementById("input")
-let btn = document.getElementById("submit-button")
+let submitBtn = document.getElementById("submit-button")
+let resetBtn = document.getElementById("reset")
 let todoList = document.getElementById("todo-list")
 
-console.dir(input)
-console.dir(btn)
-console.dir(todoList)
+// console.dir(input)
+// console.dir(submitBtn)
+// console.dir(resetBtn)
+// console.dir(todoList)
 
-btn.addEventListener('click', function(evt){
-  let li = document.createElement('li')
-  let inp = document.querySelector("input")
-  li.textContent = inp.value
-  inp.value = ""
-  document.querySelector("ul").appendChild(li)
+submitBtn.addEventListener('click', function(evt){
+  // Don’t Allow Empty Items to be Added to the To-do List
+  if (input.value){
+    let li = document.createElement('li')
+    li.setAttribute("class","todo-content")
+    let inp = document.querySelector("input")
+    li.textContent = inp.value
+    inp.value = ""
+    document.querySelector("ul").appendChild(li)
+    // console.dir(li)
+    let todoContents = document.querySelectorAll(".todo-content")
+    todoContents.forEach(function(content){
+      content.addEventListener("click",function(evt){
+        content.remove()
+      })
+    })
+  }
+})
+
+// create a reset button to reset the to-do list 
+resetBtn.addEventListener("click", function(evt){
+  let li = document.getElementById("todo-list")
+  while(li.firstChild){
+    li.removeChild(li.firstChild)
+  }
 })
