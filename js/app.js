a583fd841506c60726d3662acba4165db39af610
chenannchi <anchichenusa@gmail.com>
2022-08-30 20:16:38 -0400

    finish the lab

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-console.log("sanity check")
+let input = document.getElementById("input")
+let btn = document.getElementById("submit-button")
+let todoList = document.getElementById("todo-list")
+
+console.dir(input)
+console.dir(btn)
+console.dir(todoList)
+
+btn.addEventListener('click', function(evt){
+  let li = document.createElement('li')
+  let inp = document.querySelector("input")
+  li.textContent = inp.value
+  inp.value = ""
+  document.querySelector("ul").appendChild(li)
+})
